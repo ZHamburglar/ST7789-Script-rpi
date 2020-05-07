58e021553d3558be1172cf4725d7966dd7c8f7ac
ZHamburglar <kollinbrandenburg@gmail.com>
2020-05-07 02:04:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 import time
 import sys
 import os
-import subprocess
-
+import psutil
+import datetime
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
@@ -17,23 +17,61 @@ from PIL import Image,ImageDraw,ImageFont
 
 logging.basicConfig(level=logging.INFO)
 
+dateString = '%Y/%m/%d %H:%M:%S'
 
-try:
+print(datetime.datetime.now().strftime(dateString))
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+# % Ram Usage with psutil
+def get_ram_info():
+    """ Return RAM usage using psutil """
+    ram_cent = psutil.virtual_memory()[2]
+    return str(ram_cent)
+
+# CPU Temp in Celsius
+def get_cpu_tempfunc():
+    """ Return CPU temperature """
+    result = 0
+    mypath = "/sys/class/thermal/thermal_zone0/temp"
+    with open(mypath, 'r') as mytmpfile:
+        for line in mytmpfile:
+            result = line
+
+    result = float(result)/1000
+    result = round(result, 1)
+    return str(result)
+
+def get_gpu_tempfunc():
+    """ Return GPU temperature as a character string"""
+    res = os.popen('/opt/vc/bin/vcgencmd measure_temp').readline()
+    return res.replace("temp=", "")
+
+
+def get_cpu_use():
+    """ Return CPU usage using psutil"""
+    cpu_cent = psutil.cpu_percent()
+    return str(cpu_cent)
+
+print(get_ram_info())
+print('j',get_cpu_tempfunc())
+print('CPU Use:',get_cpu_use())
+print('GPU Temp:',get_gpu_tempfunc())
+
+disp = ST7789.ST7789()
+# Initialize library.
+disp.Init()
+
+# Clear display.
+disp.clear()
+
+while True:
     print ("2inch LCD Module")
-    disp = ST7789.ST7789()
-    # Initialize library.
-    disp.Init()
-    WIDTH = disp.width
-    HEIGHT = disp.height
-    print ('Width: ', WIDTH, '  Height: ', HEIGHT)
 
-    # Clear display.
-    disp.clear()
 
-    # image = Image.new('RGB', (disp.width,disp.height), (255,255,255))
-    # White Background 
-    image = Image.new('RGB', (disp.height,disp.width), (255,255,255)) 
-    # Black Background
+    # image = Image.new('RGB', (disp.width,disp.height), (255,255,255)) 
     image = Image.new('RGB', (disp.height,disp.width), (0,0,0)) 
 
     draw = ImageDraw.Draw(image)
@@ -41,57 +79,38 @@ try:
     font30 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 30)
     font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
     print ("***draw line")
-    # Top Side
-    draw.line([(10,10),(310,10)], fill = "BLUE",width = 5)
-    # Left side
-    draw.line([(10,10),(10,230)], fill = "BLUE",width = 5)
-    # Bottom Side
-    draw.line([(10,230),(310,230)], fill = "BLUE",width = 5)
-    # Right Side
-    draw.line([(310,10),(310,230)], fill = "BLUE",width = 5)
+    draw.line([(40,20),(200,20)], fill = "BLUE",width = 5)
+    draw.line([(40,20),(40,200)], fill = "BLUE",width = 5)
+    draw.line([(40,200),(200,200)], fill = "BLUE",width = 5)
+    draw.line([(200,20),(200,200)], fill = "BLUE",width = 5)
     print ("***draw rectangle")
     draw.rectangle([(50,30),(190,70)],fill = "BLUE")
     
-    bold = 'boooot'
-
-    # Device IP Address
-    cmd = "hostname -I | cut -d\' \' -f1"
-    IP = "IP: "+subprocess.check_output(cmd, shell=True).decode("utf-8")
-
-    cmd = "pidof hostapd | wc -l | awk '{printf \"Hotspot: %s\", $1 == 1 ? \"Active\" : \"Down\"}'"
-    Hostapd = subprocess.check_output(cmd, shell=True).decode("utf-8")
-
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %sMB %.2f%%\", $3,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
-
-    cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk \'{printf \"CPU Temp: %.1f C\", $(NF-0) / 1000}\'" # pylint: disable=line-too-long
-    Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
-
-    print (IP)
-    print (MemUsage)
-    # print ("***draw text")
-    draw.text((60,30), bold, font = font30, fill = "WHITE")
+    print ("***draw text")
+    draw.text((60,30), u'Boot Program', font = font30, fill = "WHITE")
     draw.text((50, 75), 'Waveshare Electronic ', font = font15, fill = "BLUE")
     draw.text((75, 110), '2.0inch LCD ', font = font15, fill = "BLUE")
     draw.text((72, 140), 'Test Program ', font = font15, fill = "BLUE")
-    draw.text((72, 170), Temp, font=font15, fill="#550000")
+    time.sleep(.1)
+    draw.text((72, 170), datetime.datetime.now().strftime(dateString), font = font15, fill = "BLUE")
 
 
     image=image.rotate(180) 
     disp.ShowImage(image)
-    time.sleep(3)
 
     # read bmp file 
-
-    # bmp = Image.open(os.path.join(picdir, 'machomadness.png'))	
+    # bmp = Image.open(os.path.join(picdir, 'LCD_2inch.bmp'))	
     # image.paste(bmp, (0,0))  
     # image=image.rotate(180)
     # disp.ShowImage(image)
+    time.sleep(1)
 
-except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
-    exit()
+
+
+
+
+
+# except KeyboardInterrupt:    
+#     logging.info("ctrl + c:")
+#     exit()
 
