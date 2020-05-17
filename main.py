6131c6857c0f0e8ad9faa993b5568227419ef873
ZHamburglar <kollinbrandenburg@gmail.com>
2020-05-16 22:44:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 import os
 import psutil
 import datetime
+import socket
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
@@ -17,7 +18,7 @@ from PIL import Image,ImageDraw,ImageFont
 
 logging.basicConfig(level=logging.INFO)
 
-dateString = '%Y/%m/%d %H:%M:%S'
+dateString = '%m/%d/%Y %H:%M:%S'
 
 # % Ram Usage with psutil
 def get_ram_info():
@@ -49,13 +50,26 @@ def get_cpu_use():
     cpu_cent = psutil.cpu_percent()
     return str(cpu_cent)
 
+def get_ip_address():  
+    ipaddress = psutil.net_if_addrs()['wlan0'][0].address
+    ipAddressString = 'IP Address: ' + ipaddress
+    return ipAddressString
+
 print(get_ram_info())
 print('j',get_cpu_tempfunc())
 print('CPU Use:',get_cpu_use())
 print('GPU Temp:',get_gpu_tempfunc())
 
+
+
+print(psutil.virtual_memory())  # physical memory usage
+print('memory % used:', psutil.virtual_memory()[2])
+
+print('CPU Freq: ', psutil.cpu_freq()[0])
+
 # Fonts
 font30 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 30)
+font20 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 20)
 font17 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 17)
 font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
 
@@ -67,6 +81,8 @@ disp.Init()
 disp.clear()
 
 while True:
+    print('CPU Use:',get_cpu_use())
+
     image = Image.new('RGB', (disp.height,disp.width), (0,0,0)) 
     draw = ImageDraw.Draw(image)
 
@@ -93,20 +109,18 @@ while True:
 
     draw.rectangle([(50,30),(190,70)],fill = "BLUE", outline = "WHITE")
     
-    # print ("***draw text")
-    # draw.text((60,30), u'Boot Program', font = font30, fill = "WHITE")
-    # draw.text((50, 75), 'Waveshare Electronic ', font = font15, fill = "BLUE")
-    # draw.text((75, 110), '2.0inch LCD ', font = font15, fill = "BLUE")
-    # draw.text((72, 140), 'Test Program ', font = font15, fill = "BLUE")
-    # time.sleep(.1)
-    draw.text((72, 170), datetime.datetime.now().strftime(dateString), font = font15, fill = "BLUE")
+    draw.text((72, 150), datetime.datetime.now().strftime(dateString), font = font15, fill = "BLUE")
 
     draw.rectangle([(50,30),(190,70)],fill = "BLUE", outline = "WHITE")
+    # IP Address
+    draw.text((8, 175), get_ip_address(), font = font20, fill = "BLUE")
 
+    # CPU Temperature Module
     draw.text((8, 200), cpuTempString, font = font15, fill = "BLUE")
     draw.rectangle([(90,204),(312,217)], outline = "WHITE")
     draw.rectangle([(91,205),(cpuWidth,216)], fill = "GREEN")
 
+    # GPU Temperature Module
     draw.text((8, 220), gpuTempString, font = font15, fill = "BLUE")
     draw.rectangle([(90,223),(312,236)], outline = "WHITE")
     draw.rectangle([(91,224),(gpuWidth,235)], fill = "GREEN")
@@ -120,6 +134,11 @@ while True:
     # image=image.rotate(180)
     # disp.ShowImage(image)
     time.sleep(1)
+
+
+
+
+
 
 # except KeyboardInterrupt:    
 #     logging.info("ctrl + c:")
